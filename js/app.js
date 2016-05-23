0f8fca54d5b3f6ae3cbef81826811224a5b604dd
TyraelG <tyraelgoo@gmail.com>
2016-05-22 14:02:07 -1000

    Coding Manual Changes
    Examples added

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function example1css() {
+	document.getElementById('cssExamplecss').innerHTML = "<style>#cssExample{font-family:sans-serief;color: black;background-color:white;}</style>margin-top: 100px; ";
+}
+
+function example2css() {
+  	document.getElementById('cssExamplecss').innerHTML = "<style>#cssExample{font-family:ariel;color: white;background-color:black;}</style>font-family: times;";
+}
+
+function example3css() {
+    document.getElementById('cssExamplecss').innerHTML = "<style>#cssExample{font-family:serief;color: black;background-color:white;}</style>float: right;";
+}
+
+function example4css() {
+	document.getElementById('cssExamplecss').innerHTML = "<style>#cssExample{font-family:times;color: white;background-color:black;}</style>background-color: #156b65;";
+}
+
+function example4html() {
+	document.getElementById('cssExamplecss').innerHTML = ""
+}
